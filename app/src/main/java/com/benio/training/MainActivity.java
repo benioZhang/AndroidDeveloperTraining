a4908d4b28c68afc60e125e44c5457aff48392d2
Benio_zhang <314053215@qq.com>
2016-06-23 18:22:55 +0800

    class 1

--- app/src/main/java/com/benio/training/MainActivity.java
+++ app/src/main/java/com/benio/training/MainActivity.java
@@ -1,13 +1,39 @@
 package com.benio.training;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.benio.training.class1.MyActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_class1).setOnClickListener(this);
+        findViewById(R.id.btn_class2).setOnClickListener(this);
+        findViewById(R.id.btn_class3).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_class1:
+                startActivity(new Intent(this, MyActivity.class));
+                break;
+
+            case R.id.btn_class2:
+                break;
+
+            case R.id.btn_class3:
+                break;
+
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/benio/training/class1/MyActivity.java
@@ -0,0 +1,46 @@
+package com.benio.training.class1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+
+import com.benio.training.R;
+
+public class MyActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private static final String TAG = "MyActivity";
+
+    /**
+     * Called when the user clicks the Send button
+     */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
