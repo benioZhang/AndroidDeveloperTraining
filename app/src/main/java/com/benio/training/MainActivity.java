9c158699c10d623b78fd4f75c1bf21efaddb6b6d
张智彬 <zhangzhibin@yaochufa.com>
2017-07-03 18:03:07 +0800

    MainActivity改为listView

--- app/src/main/java/com/benio/training/MainActivity.java
+++ app/src/main/java/com/benio/training/MainActivity.java
@@ -3,7 +3,13 @@ package com.benio.training;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.benio.training.class1.Class1Activity;
 import com.benio.training.class10.Class10Activity;
@@ -22,99 +28,83 @@ import com.benio.training.class7.Class7Activity;
 import com.benio.training.class8.Class8Activity;
 import com.benio.training.class9.Class9Activity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
+    DemoInfo[] mInfos = {
+            new DemoInfo("Building Your First App", Class1Activity.class),
+            new DemoInfo("Building Your First App(2)", Class2Activity.class),
+            new DemoInfo("Managing the Activity Lifecycle", Class3Activity.class),
+            new DemoInfo("Building a Dynamic UI with Fragments", Class4Activity.class),
+            new DemoInfo("Saving Data", Class5Activity.class),
+            new DemoInfo("Interacting with Other Apps", Class6Activity.class),
+            new DemoInfo("Working with System Permissions", Class7Activity.class),
+            new DemoInfo("Sharing Simple Data", Class8Activity.class),
+            new DemoInfo("Setting Up File Sharing", Class9Activity.class),
+            new DemoInfo("Sharing Files with NFC", Class10Activity.class),
+            new DemoInfo("Managing Audio Playback", Class11Activity.class),
+            new DemoInfo("Capturing Photos", Class12Activity.class),
+            new DemoInfo("Printing Content", Class13Activity.class),
+            new DemoInfo("Displaying Bitmaps Efficiently", Class14Activity.class),
+            new DemoInfo("Displaying Graphics with OpenGL ES", Class15Activity.class),
+            new DemoInfo("Animating Views Using Scenes and Transitions", Class16Activity.class)
+    };
+
+    private static class DemoInfo {
+        String title;
+        Class<?> activityClass;
+
+        public DemoInfo(String title, Class<?> activityClass) {
+            this.title = title;
+            this.activityClass = activityClass;
+        }
+    }
+
+    private static class MyAdapter extends BaseAdapter {
+        private DemoInfo[] mInfos;
+
+        public MyAdapter(DemoInfo[] infos) {
+            mInfos = infos;
+        }
+
+        @Override
+        public int getCount() {
+            return mInfos.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mInfos[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView textView;
+            if (convertView == null) {
+                convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_main, parent, false);
+            }
+            textView = (TextView) convertView.findViewById(R.id.text);
+            textView.setText(mInfos[position].title);
+            return convertView;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.btn_class1).setOnClickListener(this);
-        findViewById(R.id.btn_class2).setOnClickListener(this);
-        findViewById(R.id.btn_class3).setOnClickListener(this);
-        findViewById(R.id.btn_class4).setOnClickListener(this);
-        findViewById(R.id.btn_class5).setOnClickListener(this);
-        findViewById(R.id.btn_class6).setOnClickListener(this);
-        findViewById(R.id.btn_class7).setOnClickListener(this);
-        findViewById(R.id.btn_class8).setOnClickListener(this);
-        findViewById(R.id.btn_class9).setOnClickListener(this);
-        findViewById(R.id.btn_class10).setOnClickListener(this);
-        findViewById(R.id.btn_class11).setOnClickListener(this);
-        findViewById(R.id.btn_class12).setOnClickListener(this);
-        findViewById(R.id.btn_class13).setOnClickListener(this);
-        findViewById(R.id.btn_class14).setOnClickListener(this);
-        findViewById(R.id.btn_class15).setOnClickListener(this);
-        findViewById(R.id.btn_class16).setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_class1:
-                startActivity(new Intent(this, Class1Activity.class));
-                break;
-
-            case R.id.btn_class2:
-                startActivity(new Intent(this, Class2Activity.class));
-                break;
-
-            case R.id.btn_class3:
-                startActivity(new Intent(this, Class3Activity.class));
-                break;
-
-            case R.id.btn_class4:
-                startActivity(new Intent(this, Class4Activity.class));
-                break;
-
-            case R.id.btn_class5:
-                startActivity(new Intent(this, Class5Activity.class));
-                break;
-
-            case R.id.btn_class6:
-                startActivity(new Intent(this, Class6Activity.class));
-                break;
-
-            case R.id.btn_class7:
-                startActivity(new Intent(this, Class7Activity.class));
-                break;
-
-            case R.id.btn_class8:
-                startActivity(new Intent(this, Class8Activity.class));
-                break;
-
-            case R.id.btn_class9:
-                startActivity(new Intent(this, Class9Activity.class));
-                break;
-
-            case R.id.btn_class10:
-                startActivity(new Intent(this, Class10Activity.class));
-                break;
-
-            case R.id.btn_class11:
-                startActivity(new Intent(this, Class11Activity.class));
-                break;
-
-            case R.id.btn_class12:
-                startActivity(new Intent(this, Class12Activity.class));
-                break;
-
-            case R.id.btn_class13:
-                startActivity(new Intent(this, Class13Activity.class));
-                break;
-
-            case R.id.btn_class14:
-                startActivity(new Intent(this, Class14Activity.class));
-                break;
-
-            case R.id.btn_class15:
-                startActivity(new Intent(this, Class15Activity.class));
-                break;
-
-            case R.id.btn_class16:
-                startActivity(new Intent(this, Class16Activity.class));
-                break;
-
-            default:
-                break;
-        }
+        ListView listView = (ListView) findViewById(R.id.list);
+        final BaseAdapter adapter = new MyAdapter(mInfos);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                DemoInfo demoInfo = (DemoInfo) adapter.getItem(position);
+                startActivity(new Intent(MainActivity.this, demoInfo.activityClass));
+            }
+        });
     }
 }
